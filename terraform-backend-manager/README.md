74da047a1c068ce5c20c934646f6f676b4666f64
acrease <alex@crease.sh>
2024-08-30 18:50:24 +0100

    terraform backend manager subtree?

--- terraform-backend-manager/README.md
+++ terraform-backend-manager/README.md
@@ -1,1 +1,2 @@
-# terraform-backend-manager
+# terraform-modules
+Reusable Terraform Modules
---
+++ terraform-backend-manager/env_gen/vars.tf
@@ -0,0 +1,30 @@
+variable "environments" {
+  type        = list(string)
+  description = "A list of possible environments"
+}
+
+variable "ENV" {
+  type        = string
+  description = "Environment name to setup backend for, typically set using TF_VAR_ENV"
+  validation {
+    condition     = contains(var.environments, var.ENV)
+    error_message = "Please make sure ENV is one of the approved values: [\n${join(",\n", [for v in var.environments : "    ${v}"])}\n]"
+  }
+}
+
+variable "output_dir" {
+  type        = string
+  description = "The directory to place the output files (backend.tf, environment.auto.tfvars, & env_vars.tf.json)"
+}
+
+variable "environment_configs_dir" {
+  type        = string
+  description = "The directory where ENV-environment.yaml files are stored"
+  default     = "."
+}
+
+variable "variables" {
+  type        = map(string)
+  description = "A map of variable names and types (string formatted)"
+  default     = {}
+}

383bcaaf2a309999bdc7ecc6b2d985b4fd4b84e8
acrease <alex@crease.sh>
2024-08-31 00:11:22 +0100

    module name and versions

--- aws-backend-manager/main.tf
+++ aws-backend-manager/main.tf
@@ -6,8 +6,8 @@ locals {
 }
 
 module "s3_backend" {
-  source  = "Apollo-XIV/backend/s3"
-  version = "0.0.1"
+  source  = "Apollo-XIV/s3-backend/aws"
+  version = "0.0.3"
   prefix  = var.prefix
 
   environments  = var.environments
@@ -16,7 +16,7 @@ module "s3_backend" {
 
 module "env_gen" {
   source       = "Apollo-XIV/env-gen/local"
-  version      = "0.0.5"
+  version      = "0.0.6"
   ENV          = var.ENV
   environments = var.environments
   variables    = var.variables

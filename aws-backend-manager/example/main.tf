b52674a253a4dc449ffc447656cdb4ef494bc317
acrease <alex@crease.sh>
2024-08-31 01:21:52 +0100

    rename

--- aws-backend-manager/example/main.tf
+++ aws-backend-manager/example/main.tf
@@ -1,14 +1,14 @@
 variable "ENV" {}
 
 module "backend" {
-  source = "../."
-  # source  = "Apollo-XIV/backend-manager/aws"
-  # version = "0.0.10"
-  prefix = "backend-test"
+  # source = "../."
+  source  = "Apollo-XIV/backend-manager/aws"
+  version = "0.0.11"
+  prefix  = "backend-test"
 
-  output_dir = path.root
+  output_dir = abspath(path.root)
 
-  environment_configs_dir = path.root
+  environment_configs_dir = abspath(path.root)
   environments = [
     "dev",
     "staging"

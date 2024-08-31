4c635041846ffccd174a9ae4d21d3453104a06a1
Alex Crease <119538960+Apollo-XIV@users.noreply.github.com>
2024-08-31 14:39:03 +0100

    Update main.tf

--- aws-backend-manager/main.tf
+++ aws-backend-manager/main.tf
@@ -16,7 +16,7 @@ module "s3_backend" {
 
 module "env_gen" {
   source       = "Apollo-XIV/env-gen/local"
-  version      = "0.0.14"
+  version      = "0.0.19"
   ENV          = var.ENV
   environments = var.environments
   variables    = merge(var.variables, {

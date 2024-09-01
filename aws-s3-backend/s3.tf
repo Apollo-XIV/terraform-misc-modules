72a850e708c1c8bc6ff67bdae60388d6c0c5890e
acrease <alex@crease.sh>
2024-09-01 17:26:51 +0100

    test with change

--- aws-s3-backend/s3.tf
+++ aws-s3-backend/s3.tf
@@ -5,7 +5,7 @@ resource "aws_s3_bucket" "state" {
   #   prevent_destroy = true
   # }
 }
-# trigger changes
+# trigger change
 
 resource "aws_s3_bucket_versioning" "enabled" {
   bucket = aws_s3_bucket.state.id
